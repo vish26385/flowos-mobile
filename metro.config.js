2eebc4a898147084449b712c00066a8e6972e5af
Vishnu Pandit <vish26385@gmail.com>
2025-12-10 20:32:38 +0530

    Fix Metro config for SDK 53

--- metro.config.js
+++ metro.config.js
@@ -1,30 +1,8 @@
-// const { getDefaultConfig } = require('expo/metro-config');
-// const config = getDefaultConfig(__dirname);
-
-// config.resolver.assetExts.push('cjs'); // fixes some RN Paper web builds
-
-// module.exports = config;
-
 // metro.config.js
-const { getDefaultConfig } = require("expo/metro-config");
+const { getDefaultConfig } = require('expo/metro-config');
 
-const config = getDefaultConfig(__dirname);
+module.exports = (async () => {
+  const config = await getDefaultConfig(__dirname);
 
-// 🧩 Patch: Disable Metro’s broken symbolicator on web
-config.server = {
-  ...config.server,
-  enhanceMiddleware: (middleware) => {
-    return (req, res, next) => {
-      // Block any symbolication attempts using <anonymous> path
-      if (req.url?.includes("symbolicate")) {
-        res.end(JSON.stringify({ stack: [] }));
-        return;
-      }
-      return middleware(req, res, next);
-    };
-  },
-};
-
-module.exports = config;
-
-
+  return config;
+})();
