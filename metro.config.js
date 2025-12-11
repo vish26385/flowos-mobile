b5a7be707f93f133c0b4584a8e7435e20a78bbd6
Vishnu Pandit <vish26385@gmail.com>
2025-12-11 18:23:27 +0530

    Fix metro bundler dependency for EAS

--- metro.config.js
+++ metro.config.js
@@ -1,8 +1,6 @@
 // metro.config.js
 const { getDefaultConfig } = require('expo/metro-config');
 
-module.exports = (async () => {
-  const config = await getDefaultConfig(__dirname);
+const config = getDefaultConfig(__dirname);
 
-  return config;
-})();
+module.exports = config;
