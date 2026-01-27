4a03151e02c9a116778fc68d2a1169cc96c4aea1
Vishnu Pandit <vish26385@gmail.com>
2026-01-27 10:50:52 +0530

    Patch root Gradle to disable Kotlin Werror

--- plugins/disableKotlinWerror.js
+++ plugins/disableKotlinWerror.js
@@ -1,14 +1,50 @@
-const { withProjectBuildGradle } = require("@expo/config-plugins");
+const { withAppBuildGradle } = require("@expo/config-plugins");
+const fs = require("fs");
+const path = require("path");
 
 module.exports = function disableKotlinWerror(config) {
-  return withProjectBuildGradle(config, (config) => {
-    let contents = config.modResults.contents;
+  // Patch android/build.gradle (root) not android/app/build.gradle
+  config = withAppBuildGradle(config, (config) => {
+    // This mod targets app/build.gradle, but we need root build.gradle.
+    // We'll patch it manually using the projectRoot.
+    return config;
+  });
 
-    // Add a Kotlin compile override that disables "allWarningsAsErrors"
-    // Works for both Groovy DSL and most project setups.
-    const marker = "// --- disable-kotlin-werror ---";
-    if (!contents.includes(marker)) {
-      contents += `
+  return require("@expo/config-plugins").withDangerousMod(config, [
+    "android",
+    async (config) => {
+      const root = config.modRequest.platformProjectRoot; // .../android
+      const buildGradlePath = path.join(root, "build.gradle");
+
+      if (!fs.existsSync(buildGradlePath)) {
+        // Some projects use build.gradle.kts – handle that too
+        const ktsPath = path.join(root, "build.gradle.kts");
+        if (fs.existsSync(ktsPath)) {
+          let contents = fs.readFileSync(ktsPath, "utf8");
+          const marker = "// --- disable-kotlin-werror ---";
+          if (!contents.includes(marker)) {
+            contents += `
+
+${marker}
+subprojects {
+  tasks.withType(org.jetbrains.kotlin.gradle.tasks.KotlinCompile).configureEach {
+    compilerOptions {
+      allWarningsAsErrors.set(false)
+    }
+  }
+}
+`;
+            fs.writeFileSync(ktsPath, contents);
+          }
+        }
+        return config;
+      }
+
+      let contents = fs.readFileSync(buildGradlePath, "utf8");
+      const marker = "// --- disable-kotlin-werror ---";
+
+      if (!contents.includes(marker)) {
+        contents += `
 
 ${marker}
 subprojects { subproject ->
@@ -19,9 +55,10 @@ subprojects { subproject ->
   }
 }
 `;
-    }
+        fs.writeFileSync(buildGradlePath, contents);
+      }
 
-    config.modResults.contents = contents;
-    return config;
-  });
+      return config;
+    },
+  ]);
 };
